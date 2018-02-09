d9d5c5561f0481c37acfb3ed4946859d6c1131a4
Carlos170515 <32445274+Carlos170515@users.noreply.github.com>
2018-02-09 13:36:54 -0700

    Add files via upload

---
+++ ejemplo.php
@@ -0,0 +1,9 @@
+<form name="formulario" method="post" action="ejemploPost.php">
+
+Nombre: <input type="text" name="nombre" value="">
+
+Correo: <input type="email" name="correo" value="">
+
+<input type="submit" />
+
+</form>
---
+++ ejemploPost.php
@@ -0,0 +1,11 @@
+<?php
+
+$nombre = $_POST['nombre'];
+
+$email = $_POST['correo'];
+
+echo $nombre;
+
+echo $email;
+
+?>
